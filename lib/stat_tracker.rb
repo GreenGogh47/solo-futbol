6c5f85cf8b15387f16c5f77bf9fea26f19ba171e
Chris Crane <chrisjcrane@gmail.com>
2023-09-12 21:10:01 -0600

    StatTracker class has csv files as array of hashes

---
+++ lib/stat_tracker.rb
@@ -0,0 +1,35 @@
+require 'csv'
+
+class StatTracker
+  attr_reader :games,
+              :teams,
+              :game_teams
+
+  def initialize
+    @games = []
+    @teams = []
+    @game_teams = []
+  end
+
+  def self.from_csv(locations)
+    stat_tracker = StatTracker.new
+    
+    CSV.foreach((locations[:games]), headers: true, col_sep: ",") do |game|
+      stat_tracker.games << game.to_h
+    end
+
+    CSV.foreach((locations[:teams]), headers: true, col_sep: ",") do |team|
+      stat_tracker.teams << team.to_h
+    end
+
+    CSV.foreach((locations[:game_teams]), headers: true, col_sep: ",") do |game_team|
+      stat_tracker.game_teams << game_team.to_h
+    end
+    stat_tracker
+  end
+
+  def self.games(games)
+    
+  end
+
+end
